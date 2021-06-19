3fd61878e56543619c6b0b74d622f68a16f7b188
Icaro Miranda <icaroaugustomiranda@gmail.com>
2021-06-19 17:27:28 -0300

    criando as rotas

--- backend/src/conn.js
+++ backend/src/conn.js
@@ -1,9 +1,11 @@
-const Sequelize = require("sequelize");
+const mysql = require("mysql2");
 
-const sequelize = new Sequelize("simulador_campeonato", "admin", "adminaws", {
-    host : "database-1.cne4pisah95x.sa-east-1.rds.amazonaws.com",
-    dialect : "mysql",
-    logging : false 
-});
-module.exports = sequelize;
-global.sequelize = sequelize;
+const conn  = mysql.createConnection({
+    'user':'admin',
+    'password':'adminaws',
+    'database':'simulador_campeonato',
+    'host':'database-1.cne4pisah95x.sa-east-1.rds.amazonaws.com',
+    'port':'3306'  
+})
+
+module.exports = conn;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,36 +1,54 @@
 const express = require('express');
 const routes = express.Router();
+const mysql = require('./conn');
+const bodyParser = require('body-parser');
 
-
+routes.use(bodyParser.json());
 routes.get('/', function(req, res){
     res.send("INDEX");
 });
+
 routes.get('/novo-campeonato', function(req, res){
+
     res.send("novoCamp");
 });
-routes.get('/novo-campeonato/adicionar-equipes', function(req, res){
-    res.send("addEquipes");
+
+
+routes.post('/equipes', (req, res) => {
+    const result = req.body;
+    var idInsert = 0;
+    if (!result) {
+      return res.status(400).end();
+    }
+
+    var sql = `INSERT INTO equipes (eqp_nome, eqp_sigla) VALUES('${result.nome}', '${result.sigla}')`;
+
+    mysql.query(sql, function (err, result) {
+        if (err) {
+            return ('Falha ao inserir dados!' + err)
+        }
+        console.log('Dados inseridos com sucesso!')
+    })
+
+    return res.status(201).send("Dados inseridos com sucesso!");
+  });
+
+
+routes.get('/equipes', function(req, res){
+    mysql.query('SELECT * FROM equipes', (err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
+      });
 });
 
-routes.get('/novo-campeonato/gerar-partidas', function(req, res){
-    res.send("gerarPartidas");
-});
-
-routes.get('/gerenciar-campeonato', function(req, res){
-    res.send("gerenciarCamp");
-});
-
-routes.get('/gerenciar-campeonato/excluir-campeonato', function(req, res){
-    res.send("excluirCamp");
-});
-
-routes.get('/gerenciar-campeonato/atualizar-partidas', function(req, res){
-    res.send("atlzarPartidas");
-});
-
-routes.get('/gerenciar-campeonato/classificacao', function(req, res){
-    res.send("classificacao");
+routes.get('/campeonatos', function(req, res){
+    mysql.query('SELECT * FROM campeonatos', (err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
+      });
 });
 
 
-module.exports=routes;
+
+
+module.exports = routes;
